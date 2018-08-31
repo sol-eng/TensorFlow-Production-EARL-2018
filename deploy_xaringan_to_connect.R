2fefc1fb5ff73f99ea62acf00b031e58bd43503f
Andrie de Vries <apdevries@gmail.com>
2018-08-31 22:32:29 +0100

    First draft completed

---
+++ deploy_xaringan_to_connect.R
@@ -0,0 +1,23 @@
+# ?rsconnect::deployDoc
+
+deploy_xaringan <- function(doc){
+  qualified_doc <- normalizePath(doc, winslash = "/")
+  app_files <-  c(
+    basename(qualified_doc),
+    list.files("images", pattern = "*.png", full.names = TRUE, recursive = TRUE),
+    list.files("theme", pattern = "*.png", full.names = TRUE, recursive = TRUE)
+    # list.files("libs", full.names = TRUE, recursive = TRUE)
+  )
+  res_files <- rmarkdown::find_external_resources(qualified_doc)
+  app_files <- c(app_files, res_files$path)
+  rsconnect::deployApp(
+    appDir = dirname(qualified_doc),
+    appPrimaryDoc = basename(qualified_doc),
+    appTitle = "Taking TensorFlow into Production EARL 2018",
+    appFiles = app_files,
+    forceUpdate = TRUE,
+    server = "colorado.rstudio.com"
+  )
+}
+
+deploy_xaringan("Taking-TensorFlow-into-Production-EARL-2018.html")
